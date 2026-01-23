b8d2e799eb416a66467ea3e20e799d81710d0ef8
Лёша Кухта <akukhta@alfabank.ru>
2026-01-23 16:35:18 +0300

    new catalogs part 2

--- build.js
+++ build.js
@@ -2,6 +2,7 @@ const esbuild = require('esbuild');
 const fs = require('fs');
 const path = require('path');
 const isWatch = process.argv.includes('--watch');
+require('./scripts/prepareReferences');
 
 const common = {
   entryPoints: {
